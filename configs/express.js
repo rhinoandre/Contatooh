ec341a243d481d830207047555d2cebd64e6cc81
André Bazoli <andre.bazoli@inatel.br>
2015-12-08 12:48:27 -0200

    Fixed bug in express.js

--- configs/express.js
+++ configs/express.js
@@ -3,28 +3,28 @@ var load = require('express-load');
 var bodyParser = require('body-parser');
 
 module.exports = function(){
-	var app = express();
+    var app = express();
 
-	//enviroment configuration
-	app.set('port', 3000);
+    //enviroment configuration
+    app.set('port', 3000);
 
     //adding view engine
-	app.set('view engine', 'ejs');
+    app.set('view engine', 'ejs');
     app.set('views', './app/views');
 
-	//middleware
-	app.use(express.static('./public'));
+    //middleware
+    app.use(express.static('./public'));
 
 
-	app.use(bodyParser(urlencoded({extended:true})));
-	app.use(bodyParser.json());
-	app.use(require('method-override')());
+    app.use(bodyParser.urlencoded({extended:true}));
+    app.use(bodyParser.json());
+    app.use(require('method-override')());
 
     //initializing the home route
-	load('models', {cwd: 'app'})
-			.then('controllers')
-			.then('routes')
-			.into(app);
+    load('models', {cwd: 'app'})
+            .then('controllers')
+            .then('routes')
+            .into(app);
 
-	return app;
-}
+    return app;
+};

85d6af11388cba2e522057209dfc842760544525
André Bazoli <andre.bazoli@inatel.br>
2015-12-09 13:07:44 -0200

    Added connection between AngularJS and NodeJS

--- public/js/controllers/contatos.js
+++ public/js/controllers/contatos.js
@@ -1,26 +1,19 @@
 angular.module('contatooh')
-.controller('ContatosController', ['$scope',
-function($scope){
+.controller('ContatosController', ['$scope', '$http',
+function($scope, $http){
     $scope.total = 0;
-    $scope.contatos = [
-        {
-            _id: 1,
-            nome: 'Contato Angular 1',
-            email: 'cont1@empresa.com.br'
-        },
-        {
-            _id: 2,
-            nome: 'Contato Angular 2',
-            email: 'cont2@empresa.com.br'
-        },
-        {
-            _id: 3,
-            nome: 'Contato Angular 3',
-            email: 'cont3@empresa.com.br'
-        }
-    ];
+    $scope.contatos = [];
 
     $scope.incrementa = function(){
         $scope.total++;
     };
+
+    $http({method: 'GET', url: '/contatos'})
+        .then(function(data){
+            $scope.contatos = data.data;
+            console.log(data);
+        }).catch(function(error){
+            console.log('Não foi possível obter a lista de contatos');
+            console.log(error.statusText);
+        });
 }]);
